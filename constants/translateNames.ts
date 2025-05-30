9320198f386b33cd86859b933e59dd01eb8c8d94
Kangdy <canfrog@naver.com>
2025-05-30 12:27:51 +0900

    📝 [Docs] 이산수학 포스팅 추가 (행렬의 종류)

--- constants/translateNames.ts
+++ constants/translateNames.ts
@@ -6,7 +6,7 @@ export const translateNames: Record<string, { en: string; ko: string }> = {
   '02. Component': { en: '02. Component', ko: '02. 컴포넌트' },
 
   'discrete-mathematics': { en: 'Discrete Mathematics', ko: '이산수학' },
-  '02. Basic-Structure': { en: '02. Basic Structure', ko: '02. 기본 구조' },
+  '02. Matrix': { en: '02. Matrix', ko: '02. 행렬' },
 
   // 필요한 만큼 추가!
 };
