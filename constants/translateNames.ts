43f09929a63b6169b34cd1276ae94ee077c00f85
Kangdy <canfrog@naver.com>
2025-06-02 20:13:33 +0900

    📝 [Docs] 확률과 통계 포스팅 추가 (데이터의 정리와 요약)

--- constants/translateNames.ts
+++ constants/translateNames.ts
@@ -8,5 +8,9 @@ export const translateNames: Record<string, { en: string; ko: string }> = {
   'discrete-mathematics': { en: 'Discrete Mathematics', ko: '이산수학' },
   '02. Matrix': { en: '02. Matrix', ko: '02. 행렬' },
 
+  'probability-statistics': { en: 'Probability and Statistics', ko: '확률과 통계' },
+  '01. Data_Organization_and_Summarization' : { en : '01. Data Organization and Summarization', ko: '01. 데이터 정리와 요약'},
+  '02. Introduction_of_Probability' : { en : '02. Introduction of Probability', ko: '02. 확률의 기본 개념'}
+
   // 필요한 만큼 추가!
 };
